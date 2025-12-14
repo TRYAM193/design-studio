f126a9981629688bab85aa05b591522acd5b5e69
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-14 21:07:40 +0530

    auto-sync: 2025-12-14 21:07:40

---
+++ src/pages/DesignEditorPage.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { Provider } from 'react-redux';
+// @ts-ignore - Ignoring TS warning because store.js is JavaScript
+import { store } from '@/design-tool/redux/store';
+// @ts-ignore
+import EditorPanel from '@/design-tool/pages/Editor';
+import { VlyToolbar } from '../../vly-toolbar-readonly'; // Optional: if you want the header
+
+export default function DesignEditorPage() {
+  return (
+    // We isolate the Redux store here so it doesn't conflict with the rest of the app
+    <Provider store={store}>
+      <div className="w-full h-screen overflow-hidden bg-white">
+        {/* You can add a back button or header here if needed */}
+        <EditorPanel />
+      </div>
+    </Provider>
+  );
+}
