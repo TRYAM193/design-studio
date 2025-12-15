c09ab6b4dc32e57ab440f116ab30e6ff3dec7f65
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-15 23:03:06 +0530

    auto-sync: 2025-12-15 23:03:05

--- src/design-tool/components/CanvasEditor.jsx
+++ src/design-tool/components/CanvasEditor.jsx
@@ -176,6 +176,7 @@ export default function CanvasEditor({
           });
           store.dispatch(setCanvasObjects(newObjs))
         });
+        
         fabricCanvas.requestRenderAll();
       }
     }
