3d5354ddb7fffad7891bc00a0fbf3b27e174a49a
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-15 23:01:39 +0530

    auto-sync: 2025-12-15 23:01:39

--- src/design-tool/components/CanvasEditor.jsx
+++ src/design-tool/components/CanvasEditor.jsx
@@ -145,40 +145,41 @@ export default function CanvasEditor({
         const parsedData = typeof jsonContent === 'string' ? JSON.parse(jsonContent) : jsonContent;
       }
 
-      if (parsedData){
-      fabricCanvas.loadFromJSON(design.canvasJSON, () => { });
-      setTimeout(() => {
-        fabricCanvas.requestRenderAll();
-        const newObjs = fabricCanvas.getObjects().map((obj) => {
-          return {
-            id: obj.customId || Date.now(),
-            type: obj.textEffect === 'circle' ? 'circle-text' : obj.type,
-            ...{ src: obj.type === image ? obj.src : '' },
-            props: {
-              text: obj.text,
-              left: obj.left,
-              top: obj.top,
-              angle: obj.angle,
-              fill: obj.fill,
-              fontSize: obj.fontSize,
-              opacity: obj.opacity,
-              shadowBlur: obj.shadowBlur,
-              shadowOffsetX: obj.shadowOffsetX,
-              shadowOffsetY: obj.shadowOffsetY,
-              shadowColor: obj.shadowColor,
-              charSpacing: obj.charSpacing,
-              stroke: obj.stroke,
-              strokeWidth: obj.strokeWidth,
-              textStyle: obj.textStyle,
-              textEffect: obj.textEffect,
-              effectValue: obj.effectValue,
-              radius: obj.radius
-            }
-          };
-        });
+      if (parsedData) {
+        fabricCanvas.loadFromJSON(par, () => { });
+        setTimeout(() => {
+          fabricCanvas.requestRenderAll();
+          const newObjs = fabricCanvas.getObjects().map((obj) => {
+            return {
+              id: obj.customId || Date.now(),
+              type: obj.textEffect === 'circle' ? 'circle-text' : obj.type,
+              ...{ src: obj.type === image ? obj.src : '' },
+              props: {
+                text: obj.text,
+                left: obj.left,
+                top: obj.top,
+                angle: obj.angle,
+                fill: obj.fill,
+                fontSize: obj.fontSize,
+                opacity: obj.opacity,
+                shadowBlur: obj.shadowBlur,
+                shadowOffsetX: obj.shadowOffsetX,
+                shadowOffsetY: obj.shadowOffsetY,
+                shadowColor: obj.shadowColor,
+                charSpacing: obj.charSpacing,
+                stroke: obj.stroke,
+                strokeWidth: obj.strokeWidth,
+                textStyle: obj.textStyle,
+                textEffect: obj.textEffect,
+                effectValue: obj.effectValue,
+                radius: obj.radius
+              }
+            };
+          });
 
-        store.dispatch(setCanvasObjects(newObjs))
-      }, 90);
+          store.dispatch(setCanvasObjects(newObjs))
+        }, 90);
+      }
     }
   }, [location.state, fabricCanvas]);
 
