d3f92f6ef0d8841644cf4d067b5a829d17fb3c65
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-15 22:47:57 +0530

    auto-sync: 2025-12-15 22:47:57

--- src/design-tool/components/CanvasEditor.jsx
+++ src/design-tool/components/CanvasEditor.jsx
@@ -139,7 +139,7 @@ export default function CanvasEditor({
 
       if (design.id) setEditingDesignId(design.id);
       else setEditingDesignId(null)
-      
+      if (design.)
       fabricCanvas.loadFromJSON(design.canvasJSON, () => { });
       setTimeout(() => {
         fabricCanvas.requestRenderAll();
