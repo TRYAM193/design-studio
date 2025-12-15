f6c9a9a0a1581fcd28ce126984a4237bf3c95c4f
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-15 22:35:18 +0530

    auto-sync: 2025-12-15 22:35:18

--- src/design-tool/utils/saveDesign.js
+++ src/design-tool/utils/saveDesign.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 import { store } from '../redux/store';
 import { setCanvasObjects } from '../redux/canvasSlice';
 import { useDispatch } from 'react-redux';
-import ActionCrea 
+import ActionCreators 
 
 function removeUndefined(obj) {
   if (Array.isArray(obj)) {
