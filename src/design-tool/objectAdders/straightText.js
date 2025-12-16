a65b851ff2c429257230c9fd85172e7841268dc8
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-16 19:05:01 +0530

    auto-sync: 2025-12-16 19:05:01

--- src/design-tool/objectAdders/straightText.js
+++ src/design-tool/objectAdders/straightText.js
@@ -3,7 +3,7 @@ import { FabricText } from 'fabric';
 export default function StraightText(obj) {
   if (!obj) return;
   const props = obj.props;
-  
+
   return new FabricText(obj.props.text, {
     ...props,
     customType: 'text',
