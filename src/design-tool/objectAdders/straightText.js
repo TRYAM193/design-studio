4573d9d0e7418bf88ce700d36756648efcbf9138
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-16 18:55:40 +0530

    auto-sync: 2025-12-16 18:55:40

--- src/design-tool/objectAdders/straightText.js
+++ src/design-tool/objectAdders/straightText.js
@@ -3,7 +3,9 @@ import { FabricText } from 'fabric';
 export default function StraightText(obj) {
   if (!obj) return;
   const props = obj.props;
-  
+  const shadows = {
+    
+  }
 
   const StraightText = new FabricText(obj.props.text, {
     left: props.left,
