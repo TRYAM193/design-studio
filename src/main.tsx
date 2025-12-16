65210a5f1e191d4adfe23e9b325f1000d3128f02
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-16 19:45:01 +0530

    auto-sync: 2025-12-16 19:45:00

--- src/main.tsx
+++ src/main.tsx
@@ -57,7 +57,7 @@ createRoot(document.getElementById("root")!).render(
           <Route path="/auth" element={<AuthPage redirectAfterAuth="/dashboard" />} />
 
           <Route path="/design/*" element={<DesignEditorPage />} />
-          <Route path="/generator" element={<ThumbnailGenerator />} />
+          <Route path="/generator" element={<DesignEditorPage />} />
           {/* Dashboard Routes */}
           <Route path="/dashboard" element={<DashboardLayout />}>
             <Route index element={<DashboardHome />} />
