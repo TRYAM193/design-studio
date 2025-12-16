5778d378b91f67ab34bf7387cb3d405f1731b7a4
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-16 19:04:47 +0530

    auto-sync: 2025-12-16 19:04:46

--- src/design-tool/objectAdders/straightText.js
+++ src/design-tool/objectAdders/straightText.js
@@ -3,30 +3,10 @@ import { FabricText } from 'fabric';
 export default function StraightText(obj) {
   if (!obj) return;
   const props = obj.props;
-  const shadows = {
-    color: 
-  }
-
-  const StraightText = new FabricText(obj.props.text, {
-    left: props.left,
-    top: props.top,
-    width: props.width,
-    height: props.height,
-    opacity: props.opacity,
-    fontFamily: props.fontFamily,
-    fontSize: props.fontSize,
-    fill: props.fill,
-    charSpacing: props.charSpacing || 0, 
+  
+  return new FabricText(obj.props.text, {
+    ...props,
     customType: 'text',
     customId: obj.id
   });
-
-  if (props.stroke || props.strokeWidth){
-    StraightText.set({
-      stroke: props.stroke || 'white',
-      strokeWidth: props.strokeWidth || 0
-    })
-  }
-
-  if (props.shadowColor || )
 }
