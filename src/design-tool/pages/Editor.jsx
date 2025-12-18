f0f18fe5fe1de247d4a29123dddf678e43471f08
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-18 19:00:56 +0530

    auto-sync: 2025-12-18 19:00:56

--- src/design-tool/pages/Editor.jsx
+++ src/design-tool/pages/Editor.jsx
@@ -8,7 +8,7 @@ import SaveDesignButton from '../components/SaveDesignButton';
 import RightSidebarTabs from '../components/RightSidebarTabs';
 import { undo, redo } from '../redux/canvasSlice';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, useLocation, useSearchParams } from 'react-router-dom';
+import { useNavigate, useLocation, useSearchParams } from 'react-router';
 import { useAuth } from '@/hooks/use-auth';
 import MainToolbar from '../components/MainToolbar';
 import ContextualSidebar from '../components/ContextualSidebar';
