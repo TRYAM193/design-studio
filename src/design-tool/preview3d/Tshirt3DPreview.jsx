c49794d1ca47123729c06b4fc764ea4fb91a447d
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-21 23:06:07 +0530

    auto-sync: 2025-12-21 23:06:07

--- src/design-tool/preview3d/Tshirt3DPreview.jsx
+++ src/design-tool/preview3d/Tshirt3DPreview.jsx
@@ -60,7 +60,7 @@ function TshirtModel({ productId, textures, color, controls }) {
     <group 
       position={[0, -0.85, 0]} 
       scale={0.8} 
-      rotation={[0, 0, 0]} 
+      rotation={[0.1, 0, 0]} 
     >
       {/* --- FRONT MESH --- */}
       <mesh geometry={nodes?.[m.front]?.geometry}>
