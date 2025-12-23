e86b009ed05e3ea304cafd40a6b469647f8a9de2
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-23 22:17:17 +0530

    auto-sync: 2025-12-23 22:17:17

--- src/data/initialProducts.ts
+++ src/data/initialProducts.ts
@@ -260,7 +260,9 @@ export const INITIAL_PRODUCTS = [
     
     print_areas: {
       front: { width: 4000, height: 4000 },
-      back:  { width: 4500, height: 5400 }
+      back:  { width: 4500, height: 5400 },
+      leftSleeve: { width: 1100, height: 3500 },
+      rightSleeve: { width: 1100, height: 3500 }
     },
 
     mockups: {
