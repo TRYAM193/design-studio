63cdb34665f760851716c114d197391609355424
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-23 20:14:14 +0530

    auto-sync: 2025-12-23 20:14:14

--- src/data/initialProducts.ts
+++ src/data/initialProducts.ts
@@ -65,7 +65,7 @@ export const INITIAL_PRODUCTS = [
     },
 
    mockups: {
-      front: "/assets/mockups/men-hoodie-front.jpg",
+      front: "/assets/mockups/men-oversized-front.jpg",
       back: "/assets/mockups/men-hoodie-back.jpg"
     },
 
