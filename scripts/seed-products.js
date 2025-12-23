fc266474e7c76df827e0ff90c2442ac2caf55aa5
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-23 20:44:50 +0530

    auto-sync: 2025-12-23 20:44:50

--- scripts/seed-products.js
+++ scripts/seed-products.js
@@ -1,7 +1,7 @@
 // scripts/seed-products.js
 import { db } from "../src/firebase"; // Ensure this points to your firebase config
 import { doc, setDoc, writeBatch, collection } from "firebase/firestore";
-import 
+import INI
 
 // --- 1. THE MASTER CATALOG DATA ---
 export const CATALOG_DATA = [
