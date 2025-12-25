31c205eb6a27e06815a76dd2ef8b6b69fa3721bb
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-25 20:17:01 +0530

    auto-sync: 2025-12-25 20:17:01

--- src/data/initialProducts.ts
+++ src/data/initialProducts.ts
@@ -35,8 +35,8 @@ export const INITIAL_PRODUCTS = [
     
     // 📐 2D POSITIONING (Top/Left/Width %)
     print_area_2d: {
-      front: { top: 20, left: 30, width: 40 },
-      back:  { top: 20, left: 30, width: 40 }
+      front: { top: 30, left: 30, width: 40 },
+      back:  { top: 30, left: 30, width: 40 }
     },
 
     options: {
