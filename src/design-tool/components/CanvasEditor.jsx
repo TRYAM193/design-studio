acb57cdebb7c9dc76fa8900cb0e384dc11495d30
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-26 20:16:25 +0530

    auto-sync: 2025-12-26 20:16:25

--- src/design-tool/components/CanvasEditor.jsx
+++ src/design-tool/components/CanvasEditor.jsx
@@ -614,7 +614,7 @@ export default function CanvasEditor({
           try {
             const newObj = await FabricImage.fromURL(objData.src, { ...objData.props, customId: objData.id });
             fabricCanvas.add(newObj);
-            console.log('Image added', onj)
+            console.log('Image added', objData)
           } catch (err) {
             console.error("Error loading image:", err);
           }
