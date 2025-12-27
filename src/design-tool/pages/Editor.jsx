eaf5659b6304acc1bc445350f606412590f2782d
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-27 18:35:03 +0530

    auto-sync: 2025-12-27 18:35:03

--- src/design-tool/pages/Editor.jsx
+++ src/design-tool/pages/Editor.jsx
@@ -467,7 +467,7 @@ export default function EditorPanel() {
                         setSelectedId={setSelectedId}
                         fabricCanvas={fabricCanvas}
                         printDimensions={canvasDims}
-                        productId={productData.productId}
+                        productId={productData.id}
                         activeView={currentView}
                     />
                 </main>
