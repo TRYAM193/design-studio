130f65e46751888e1a63725ae4337f1058b3e854
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-27 20:17:17 +0530

    auto-sync: 2025-12-27 20:17:17

--- src/design-tool/components/ContextualSidebar.jsx
+++ src/design-tool/components/ContextualSidebar.jsx
@@ -4,7 +4,7 @@ import ShapesSidebar from './ShapesSidebar';
 import { FiType, FiCircle, FiSunrise, FiFlag } from 'react-icons/fi';
 import SidebarSavedList from './SidebarSavedList';
 
-export default function ContextualSidebar({ activePanel, setActivePanel, addText, addHeading, addSubheading }) {
+export default function ContextualSidebar({ activePanel, setActivePanel, addText, addHeading, addSubheading, product }) {
 
   let ContentComponent = null;
   let title = "";
