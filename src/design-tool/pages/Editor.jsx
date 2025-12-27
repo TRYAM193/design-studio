f4b628303298b0d8b57820e63f7e20f01b20e0b2
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-27 17:32:10 +0530

    auto-sync: 2025-12-27 17:32:10

--- src/design-tool/pages/Editor.jsx
+++ src/design-tool/pages/Editor.jsx
@@ -270,6 +270,7 @@ export default function EditorPanel() {
     const handleColorChange = (colorName) => {
         const hex = COLOR_MAP[colorName] || colorName;
         setCanvasBg(hex);
+        setIsActive(true);
     };
 
     const handleGeneratePreview = () => {
