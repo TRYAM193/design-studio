45ca136cf8ab3bc45abf02b066e99a135ad9d2a1
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-28 21:24:55 +0530

    auto-sync: 2025-12-28 21:24:55

--- src/design-tool/components/Toolbar.jsx
+++ src/design-tool/components/Toolbar.jsx
@@ -180,7 +180,7 @@ export default function Toolbar({ id, type, object, updateObject, removeObject, 
       const fabricObj = fabricCanvas.getObjects().find((o) => o.customId === id);
 
       if(fabricObj) {
-        const imgElement = new Ima
+        const imgElement = new Image()
     }
   }
 
