d4fe4c5519e8181d0b58a990009af9365b261128
Shreyas <shreyaskumarswamy2007@gmail.com>
2025-12-30 22:43:10 +0530

    auto-sync: 2025-12-30 22:43:10

--- src/data/initialProducts.ts
+++ src/data/initialProducts.ts
@@ -93,9 +93,9 @@ export const INITIAL_PRODUCTS = [
     price: {
       IN: 899,
       US: 39.99,
-      GB: 29.99,
-      EU: 34.99,
-      CA: 49.99
+      GB: 16.99
+EU: 18.99,
+CA: 37.99
     },
 
     print_area_2d: {
