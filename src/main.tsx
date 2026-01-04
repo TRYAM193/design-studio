9517a9a631f217b90484e505648a619ed0e2b117
Shreyas <shreyaskumarswamy2007@gmail.com>
2026-01-04 12:35:30 +0530

    auto-sync: 2026-01-04 12:35:29

--- src/main.tsx
+++ src/main.tsx
@@ -57,7 +57,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <VlyToolbar />
     <AuthProvider>
-      <CartProvider>
+      {/* <CartProvider> */}
         <BrowserRouter>
           <RouteSyncer />
           <Routes>
@@ -90,7 +90,7 @@ createRoot(document.getElementById("root")!).render(
           </Routes>
         </BrowserRouter>
         <Toaster />
-      </CartProvider>
+      {/* </CartProvider> */}
     </AuthProvider>
   </StrictMode>,
 );
