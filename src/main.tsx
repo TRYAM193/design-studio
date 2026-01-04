a8bf477e6a7f70f18cd5a7b48345870125543e97
Shreyas <shreyaskumarswamy2007@gmail.com>
2026-01-04 19:27:35 +0530

    auto-sync: 2026-01-04 19:27:35

--- src/main.tsx
+++ src/main.tsx
@@ -28,7 +28,7 @@ import { CartProvider } from "./context/CartContext";
 import CartPage from "./pages/CartPage.tsx"; // ✅ Import Provider
 import AdminOrders from "./pages/AdminOrders.tsx";
 import ProductDetails from "./pages/ProductDetails";
-import He
+import HeadlessRender from "./pages/HeadlessRender.tsx";
 
 function RouteSyncer() {
   const location = useLocation();
@@ -86,7 +86,7 @@ createRoot(document.getElementById("root")!).render(
 
             <Route path="/checkout" element={<OrderCheckoutPage />} />
             <Route path="/admin/orders" element={<AdminOrders />} />
-            <Route path="/admin/orders" element={<AdminOrders />} />
+            <Route path="/admin/orders" element={<HeadlessRender />} />
 
             <Route path="*" element={<NotFound />} />
           </Routes>
